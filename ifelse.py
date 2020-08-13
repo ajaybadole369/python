a6b16c06c1e8c0dd7f69d2a11b4b2041ba267956
ajaybadole369 <ajaybadole654@gmail.com>
2020-08-13 10:23:44 +0530

    if else

--- ifelse.py
+++ ifelse.py
@@ -1,3 +1,7 @@
-num=int(input('enter no '))
-if num%2==0: print('no is even')
-else: print('no is odd')
+# num=int(input('enter no '))
+# if num%2==0: print('no is even')
+# else: print('no is odd')
+a,b=12,32
+z=1+(a if a<b else b)+2
+
+print(z)# result :15
